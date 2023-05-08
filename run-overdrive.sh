40343db64fc2560db38a117e81008f1b3d411485
Gavin Tronset <gtronset@gmail.com>
2023-05-08 12:22:49 -0700

    Add Pre-Commit & Linting (#10)
    This adds Pre-Commit and several linting actions:
    
    * Typo / spellcheck
    * Markdown lint
    * Shellcheck

--- run-overdrive.sh
+++ run-overdrive.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ ! -z $DESTINATION_FOLDER ]]; then
-    cd $DESTINATION_FOLDER
+if [[ -n $DESTINATION_FOLDER ]]; then
+    cd "$DESTINATION_FOLDER" || (echo "Directory does not exist" && return)
 fi
 
 SOURCE_PREPEND=""
 
-if [[ ! -z $SOURCE_FOLDER ]]; then
+if [[ -n $SOURCE_FOLDER ]]; then
     SOURCE_PREPEND=$SOURCE_FOLDER/
 fi
 
